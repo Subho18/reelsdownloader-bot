26065dbd8f592261793167780a1050bf926b276f
SUBHABRATA HORE <91530594+Subho18@users.noreply.github.com>
2026-02-03 21:20:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,5 +22,6 @@ app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT, handle_message))
+print("✅ Bot started successfully...")
 
 app.run_polling()
